c9f1e3d4070f91ac5ed271d0f317b1472d75ad8d
Thomas Hagerty <thomashagerty@pobox.com>
2025-03-16 14:54:32 +0100

    fix plots and measurement.csv

--- plot.r
+++ plot.r
@@ -3,18 +3,23 @@ library(ggplot2)
 
 measure <- read.csv('measurements.csv')
 measure$protocol_variant <- ifelse(measure$variant=="", measure$protocol, paste(measure$protocol, measure$variant, sep="-"))
-quit()
 measure$dig_begin <- as.POSIXct(measure$dig_begin)
 measure$dig_end <- as.POSIXct(measure$dig_end)
 measure$dig_duration <- as.numeric(measure$dig_end - measure$dig_begin, units="secs")
 measure$ok <- measure$dig_out != ""
 
-p <- ggplot(measure) +
-  aes(x=protocol_variant, y=dig_duration, color=ok) +
+p <- ggplot(subset(measure, ok)) +
+  aes(x=protocol_variant, y=dig_duration) +
   coord_flip() +
   labs(title="Variants of anonymous DNS lookup") +
   scale_x_discrete("DNS lookup protocol variant") +
   scale_y_continuous("Time of dig execution") +
   geom_point(alpha=0.2)
-
 ggsave("/tmp/p1.pdf")
+
+p <- ggplot(measure) +
+  aes(x=protocol_variant, fill=ok) +
+  scale_x_discrete("DNS lookup protocol variant") +
+  scale_y_continuous("Time of dig execution") +
+  geom_bar()
+ggsave("/tmp/p2.pdf")
